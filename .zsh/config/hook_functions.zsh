16ee986a6401b5aae8ea725455279d3932b765b0
Johnny Crash <cannon.imus@gmail.com>
2014-04-08 23:41:15 +0200

    Better update.

--- .zsh/config/hook_functions.zsh
+++ .zsh/config/hook_functions.zsh
@@ -1,10 +1,22 @@
-chpwd_functions=("change_terminal_title")
+typeset -ga preexec_functions
+typeset -ga precmd_functions
+typeset -ga chpwd_functions
 
 # When directory is changed set xterm title to host:dir
-change_terminal_title() {
+update_terminal_title() {
     [[ -t 1 ]] || return
     case $TERM in
         sun-cmd) print -Pn "\e]l%~\e\\";;
         *xterm*|*rxvt*|(dt|k|E)term) print -Pn "\e]2;%m:%~\a";;
     esac
 }
+
+precmd_update_terminal_title() {
+    update_terminal_title
+}
+precmd_functions+="precmd_update_terminal_title"
+
+chpwd_update_terminal_title() {
+    update_terminal_title
+}
+chpwd_functions+="chpwd_update_terminal_title"
